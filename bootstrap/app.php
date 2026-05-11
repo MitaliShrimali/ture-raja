98f90ac62dcd9c6e69dc092e6f58406f6c6d310c
MitaliShrimali <mitalishrimali0808@gmail.com>
2026-05-11 14:42:14 +0530

    fix: correct bootstrap/app.php public path

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,5 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })
-    ->usePublicPath(__DIR__.'/..')
     ->create();
+
+$app->usePublicPath(__DIR__.'/..');
+
+return $app;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@500;600;700;800&family=Plus+Jakarta+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
 
     <!-- Styles & Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'], 'build')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     
     <!-- Lordicon for animated icons -->
     <script src="https://cdn.lordicon.com/xdjxvujz.js"></script>
