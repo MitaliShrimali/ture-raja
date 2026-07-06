97c4528e074a9a2efd143f564b313d70931e74d5
MitaliShrimali <mitalishrimali0808@gmail.com>
2026-07-06 12:11:38 +0530

    Set up automated Hostinger deployment pipeline

--- public/index.php
+++ public/index.php
@@ -8,13 +8,24 @@ define('LARAVEL_START', microtime(true));
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
+} elseif (file_exists($maintenance = __DIR__.'/../../tour-raja/storage/framework/maintenance.php')) {
+    require $maintenance;
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+    require __DIR__.'/../vendor/autoload.php';
+} else {
+    require __DIR__.'/../../tour-raja/vendor/autoload.php';
+}
 
 // Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
+if (file_exists(__DIR__.'/../bootstrap/app.php')) {
+    /** @var Application $app */
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+} else {
+    /** @var Application $app */
+    $app = require_once __DIR__.'/../../tour-raja/bootstrap/app.php';
+}
 
 $app->handleRequest(Request::capture());
